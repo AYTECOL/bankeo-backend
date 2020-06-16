8b371fcc6840868f57b5d010cbb6e15876f3eb3b
Esteban Ceron <restebance@gmail.com>
2020-06-16 14:40:16 -0500

    [FEATURE] fetch image from url endpoint

--- handler.js
+++ handler.js
@@ -1,18 +1,18 @@
 'use strict';
+const fileUploader = require('./lib/fileUploader');
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.upload = async event => {
+  let userData = JSON.parse(event.body);
+  let requestPayload = userData.data;
+  let statusCode = 200;
+  let data = {};
+  try {
+    data = await fileUploader.uploadFileToBucket(requestPayload.user_image_key, requestPayload.user_image_url);
+    console.log(data)
+  } catch(error){
+    console.log(error);
+    data = {'error': 'could not upload the file to s3'};
+    statusCode = 500;
+  }
+  return {statusCode: statusCode, body: JSON.stringify({data}, null, 2),};
 };
